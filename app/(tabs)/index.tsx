62a8a2030fc063898a108106bf87bb6b5da180f5
tolzzyyy <williamsegberongbe@gmail.com>
2025-11-07 04:13:26 +0100

    fixes3

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import SafeAreaScreen from '@/components/SafeAreaScreen';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import React from 'react';
 import {
   FlatList,
   ScrollView,
