92fce2fd090fd7b8c022d21f0c5f9df30250c131
Lex043 <alexferanmi390@gmail.com>
2026-02-20 13:55:30 +0100

    fix: barcharts and padding

--- app/admin/(tabs)/index.tsx
+++ app/admin/(tabs)/index.tsx
@@ -7,6 +7,7 @@ import { ImageBackground } from "expo-image";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const matches = [
   {
@@ -47,47 +48,46 @@ const matches = [
 export default function AdminHomeScreen() {
   return (
     <View className="flex-1">
-      <ImageBackground
-        style={{ paddingTop: 20 }}
-        source={require("@/assets/images/adminHeader.png")}
-      >
-        <StatusBar style="auto" />
-        <View className="px-[35px] pt-5 pb-6">
-          <View className="flex flex-row items-center justify-between">
-            <Text
-              style={{ fontFamily: "Poppins_600SemiBold" }}
-              className="text-white text-xs bg-[#FFFFFF33] rounded-[10px] p-[10px]"
-            >
-              Pitch Condition: Excellent
-            </Text>
-            <View className="flex flex-row items-center gap-2">
-              <TouchableOpacity className="bg-[#FFFFFF33] py-2 px-2 rounded-[10px]">
-                <SettingsIcon width={16} height={16} color="#2D264B" />
-              </TouchableOpacity>
-
-              <TouchableOpacity className="bg-[#FFFFFF33] py-2 px-2 rounded-[10px]">
-                <Text>🌤️</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-          <View className="mt-[105px]">
-            <View className="flex flex-row items-center gap-2 bg-[#FFFFFF33] w-[210px] rounded-[10px] pl-[5px] py-[10px]">
-              <LocationIcon />
+      <ImageBackground source={require("@/assets/images/adminHeader.png")}>
+        <SafeAreaView edges={["top"]}>
+          <StatusBar style="auto" />
+          <View className="px-[35px] pt-5 pb-6">
+            <View className="flex flex-row items-center justify-between">
               <Text
                 style={{ fontFamily: "Poppins_600SemiBold" }}
-                className="text-white text-xs "
+                className="text-white text-xs bg-[#FFFFFF33] rounded-[10px] p-[10px]"
               >
-                Lakowe Lakes, Lakowe, Lagos
+                Pitch Condition: Excellent
+              </Text>
+              <View className="flex flex-row items-center gap-2">
+                <TouchableOpacity className="bg-[#FFFFFF33] py-2 px-2 rounded-[10px]">
+                  <SettingsIcon width={16} height={16} color="#2D264B" />
+                </TouchableOpacity>
+
+                <TouchableOpacity className="bg-[#FFFFFF33] py-2 px-2 rounded-[10px]">
+                  <Text>🌤️</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View className="mt-[105px]">
+              <View className="flex flex-row items-center gap-2 bg-[#FFFFFF33] w-[210px] rounded-[10px] pl-[5px] py-[10px]">
+                <LocationIcon />
+                <Text
+                  style={{ fontFamily: "Poppins_600SemiBold" }}
+                  className="text-white text-xs "
+                >
+                  Lakowe Lakes, Lakowe, Lagos
+                </Text>
+              </View>
+              <Text
+                style={{ fontFamily: "PlayfairDisplay_700Bold" }}
+                className="text-white text-[50px] uppercase"
+              >
+                Ant Sports
               </Text>
             </View>
-            <Text
-              style={{ fontFamily: "PlayfairDisplay_700Bold" }}
-              className="text-white text-[50px] uppercase"
-            >
-              Ant Sports
-            </Text>
           </View>
-        </View>
+        </SafeAreaView>
       </ImageBackground>
       <SafeAreaScreen className="flex-1">
         <ScrollView
